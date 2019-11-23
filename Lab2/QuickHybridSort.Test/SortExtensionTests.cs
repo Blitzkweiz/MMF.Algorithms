7b9fd9643aa51f58920f9ff0bc02d3d775a7861e
Blitzkweiz <bezlikiy.pianist@gmail.com>
2019-11-24 00:16:04 +0300

    Sorting methods created. Tests for calculating the time spent created.

---
+++ Lab2/QuickHybridSort.Test/SortExtensionTests.cs
@@ -0,0 +1,100 @@
+using System;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Threading;
+
+namespace QuickHybridSort.Test
+{
+    public class Tests
+    {
+        [TestCase(100000, 100000, 12)]
+        [TestCase(100000, 1000000, 12)]
+        [TestCase(100000, 10000000, 12)]
+        [TestCase(1000000, 100000, 12)]
+        [TestCase(1000000, 1000000, 12)]
+        [TestCase(1000000, 10000000, 12)]
+        [TestCase(10000000, 100000, 12)]
+        [TestCase(10000000, 1000000, 12)]
+        [TestCase(10000000, 10000000, 12)]
+        public void HybridSortTest(int length, int maxElement, int k)
+        {
+            var sw = new Stopwatch();
+            var rand = new Random();
+            int[] actual1 = new int[length];
+            int[] actual2 = new int[length];
+            int[] actual3 = new int[length];
+            int[] expected = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                actual1[i] = rand.Next(maxElement);
+            }
+            Array.Copy(actual1, expected, actual1.Length);
+            Array.Copy(actual1, actual2, actual1.Length);
+            Array.Copy(actual1, actual3, actual1.Length);
+            
+            sw.Start();
+            SortExtension.HybridSort(actual1, 0, actual1.Length - 1, k);
+            sw.Stop();
+            System.Console.WriteLine($"HybridSort time is {sw.ElapsedMilliseconds}ms");
+            
+            sw.Restart();
+            SortExtension.RightQuickSort(actual2, 0, actual2.Length - 1);
+            sw.Stop();
+            System.Console.WriteLine($"RightQuickSort time is {sw.ElapsedMilliseconds}ms");
+            
+            sw.Restart();
+            SortExtension.RandomQuickSort(actual3, 0, actual3.Length - 1);
+            sw.Stop();
+            System.Console.WriteLine($"RandomQuickSort time is {sw.ElapsedMilliseconds}ms");
+            
+            sw.Restart();
+            Array.Sort(expected);
+            sw.Stop();
+            System.Console.WriteLine($"Library sort time is {sw.ElapsedMilliseconds}ms");
+            
+            Assert.AreEqual(expected, actual1);
+            Assert.AreEqual(expected, actual2);
+            Assert.AreEqual(expected, actual3);
+        }
+
+        [Test]
+        public void MudrostTest()
+        {
+            System.Console.WriteLine($"Spas mudrost: {new Random().Next(1, 20)}");
+        }
+
+        [TestCase(100000, 100000, 12)]
+        [TestCase(100000, 1000000, 12)]
+        [TestCase(100000, 10000000, 12)]
+        [TestCase(1000000, 100000, 12)]
+        [TestCase(1000000, 1000000, 12)]
+        [TestCase(1000000, 10000000, 12)]
+        [TestCase(10000000, 100000, 12)]
+        [TestCase(10000000, 1000000, 12)]
+        [TestCase(10000000, 10000000, 12)]
+        public void HybridKTest(int length, int maxElement, int k)
+        {
+            var sw = new Stopwatch();
+            var rand = new Random();
+            int[] actual1 = new int[length];
+            int[] expected = new int[length];
+            for (var i = 0; i < length; i++)
+            {
+                actual1[i] = rand.Next(maxElement);
+            }
+            Array.Copy(actual1, expected, actual1.Length);
+            
+            sw.Start();
+            SortExtension.HybridSort(actual1, 0, actual1.Length - 1, k);
+            sw.Stop();
+            System.Console.WriteLine($"HybridSort time is {sw.ElapsedMilliseconds}ms");
+            
+            sw.Restart();
+            Array.Sort(expected);
+            sw.Stop();
+            System.Console.WriteLine($"Library sort time is {sw.ElapsedMilliseconds}ms");
+            
+            Assert.AreEqual(expected, actual1);
+        }
+    }
+}
---
+++ Lab2/QuickHybridSort/SortExtension.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading;
+
+namespace QuickHybridSort
+{
+    public static class SortExtension
+    {
+        public static void RightQuickSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                var pivot = RightPartition(array, start, end);
+                RightQuickSort(array, start, pivot - 1);
+                RightQuickSort(array, pivot + 1, end);
+            }
+        }
+
+        public static void RandomQuickSort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                var pivot = RandomPartition(array, start, end);
+                RandomQuickSort(array, start, pivot - 1);
+                RandomQuickSort(array, pivot + 1, end);
+            }
+        }
+
+        public static int RightPartition (int[] array, int start, int end) 
+        {
+            int temp;
+            var marker = start;
+            for ( var i = start; i < end; i++ ) 
+            {
+                if ( array[i] < array[end] ) 
+                {
+                    temp = array[marker]; 
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            temp = array[marker];
+            array[marker] = array[end];
+            array[end] = temp; 
+            return marker;
+        }
+
+        public static int RandomPartition(int[] array, int start, int end)
+        {
+            var rand = new Random();
+            var pivot = array[rand.Next(start, end)];
+
+            int temp;
+            var i = start;
+            var j = end;
+
+            while (true)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (i >= j)
+                {
+                    return j;
+                }
+
+                if (array[i] != array[j])
+                {
+                    temp = array[i];
+                    array[i] = array[j]; 
+                    array[j] = temp;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] array, int start, int end)
+        {
+            int value;
+            var i = start;
+            int j;
+            while (i <= end)
+            {
+                value = array[i];
+                j = i;
+                while (j > start && array[j - 1] > value)
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+
+                array[j] = value;
+                i++;
+            }
+        }
+
+        public static void HybridSort(int[] array, int start, int end, int k)
+        {
+            if (start < end)
+            {
+                if (end - 1 > k)
+                {
+                    var sep = RightPartition(array, start, end);
+                    HybridSort(array, start, sep - 1, k);
+                    HybridSort(array, sep + 1, end, k);
+                }
+                else
+                {
+                    InsertionSort(array, start, end);
+                }
+            }
+        }
+    }
+}
